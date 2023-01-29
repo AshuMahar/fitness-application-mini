9f655c730fd6f02351d77ea725366bc706105ed4
AshuMahar <ashumahar69@gmail.com>
2023-01-29 12:48:06 +0530

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '5ffd7baa04msh3ffd4308350322fp1cffb7jsnab4facfc675d',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
